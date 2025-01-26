3f08fdccd28badf0c1a3721e5b17d926b700e445
esbenholk <esbenholknielsen@gmail.com>
2025-01-26 15:45:02 +0100

    heroku setup

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("b179876d02ce866b")
+/******/ 		__webpack_require__.h = () => ("7a37b7b91e8e7dcd")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
